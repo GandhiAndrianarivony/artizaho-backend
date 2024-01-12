dce12e700682b813cffd1d6b35cf1aa8e8732b8c
GandhiAndrianarivony <gandhi.andrianarivony@gmail.com>
2024-01-12 12:20:04 +0300

    push image to docker registry

--- apps/users/admin.py
+++ apps/users/admin.py
@@ -1,3 +1,14 @@
-from django.contrib import admin
+"""Module used to display users information on django admin UI
+"""
 
+
+from django.contrib import admin
+from . import models
+
+
+class UserAdmin(admin.ModelAdmin):
+    list_display = ("id", "email", "username", "type", "gender", "dob")
+
+
+admin.site.register(models.User, UserAdmin)
 # Register your models here.
